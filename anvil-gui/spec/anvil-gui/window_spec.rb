6eeaba4f5dad7a73e21a7c70ed2bd7a87eb1a378
Lance Carlson <lancecarlson@gmail.com>
2008-04-28 03:49:04 -0400

    Small edits

--- anvil-gui/spec/anvil-gui/window_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", "spec_helper")
-
-include Anvil::GUI
-
-describe Anvil::GUI, "window" do  
-  it "should display an error message if the dependency is missing" do
-    window "Test Window"
-  end
-end
